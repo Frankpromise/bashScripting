4aaeccbef128df150e1b1c23a8e940f96a06c552
promise <promise@DESKTOP-5V7NG46.localdomain>
2023-10-11 09:43:57 +0200

    commit changes to file

--- git-commit.sh
+++ git-commit.sh
@@ -2,7 +2,7 @@
 
 ################################################################################
 # Script Name    : git-commit.sh
-# Description    : bash script use  to git commit changes to single files
+# Description    : Bash script used to git commit changes to single files
 # Args           : FILE COMMIT_MESSAGE
 ##############################################################################
 
@@ -10,40 +10,62 @@ set -o errexit
 set -o pipefail
 set -o nounset
 
-function log() {
-    local MESSAGE="${1}"
-    echo "${MESSAGE}" 1>&2
+function log_message() {
+    local message="${1}"
+    echo "${message}" 1>&2
 }
 
-function usage() {
-    log "ERROR: Missing or invalid arguments"
-    log "Usage example: ${0} FILE COMMIT_MESSAGE"
+function display_usage_and_exit() {
+    log_message "ERROR: Missing or invalid arguments"
+    log_message "Usage example: ${0} FILE COMMIT_MESSAGE"
     exit 1
 }
 
-if [[ "${#}" -ne 2 ]]; then
-    usage
-fi
+function check_for_file_changes() {
+    local file="${1}"
+    local diff
+    diff=$(git diff "${file}")
 
-FILE="${1}"
-COMMIT_MESSAGE="${2}"
+    if [[ -z "${diff}" ]]; then
+        log_message "ERROR: No changes to commit"
+        exit 1
+    else
+        log_message "INFO: The following changes have been made to the ${file} file:"
+        log_message "${diff}"
+    fi
+}
 
-DIFF=$(git diff "${FILE}")
+function set_author_info_for_commit() {
+    local author_name
+    local author_email
+    
+    author_name=$(git log -1 --pretty=format:'%an')
+    author_email=$(git log -1 --pretty=format:'%ae')
 
-if [[ -z "${DIFF}" ]]; then
-    log "ERROR: No changes to commit"
-    exit 1
-else
-    log "INFO: The following changes have been made to the ${FILE} file:"
-    log "${DIFF}"
+    log_message "INFO: Using as author: ${author_name} <${author_email}>"
+    git config user.name "${author_name}"
+    git config user.email "${author_email}"
+}
 
-    AUTHOR_NAME=$(git log -1 --pretty=format:'%an')
-    AUTHOR_EMAIL=$(git log -1 --pretty=format:'%ae')
+function perform_git_commit() {
+    local file="${1}"
+    local commit_message="${2}"
 
-    log "INFO: Using as author: ${AUTHOR_NAME} <${AUTHOR_EMAIL}>"
-    git config user.name "${AUTHOR_NAME}"
-    git config user.email "${AUTHOR_EMAIL}"
+    git add "${file}"
+    git commit -m "${commit_message}"
+}
 
-    git add "${FILE}"
-    git commit -m "${COMMIT_MESSAGE}"
-fi
+function main() {
+    if [[ "${#}" -ne 2 ]]; then
+        display_usage_and_exit
+    fi
+    
+    local file="${1}"
+    local commit_message="${2}"
+
+    check_for_file_changes "${file}"
+    set_author_info_for_commit
+    perform_git_commit "${file}" "${commit_message}"
+}
+
+main "${@}"
